7cb394991e93576ba190d1676dda9c2062b4cd38
Katayama Shinichi <shin1.katayama@gmail.com>
2012-02-04 16:51:53 +0900

    Create init script.

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+DROPBOX_DIR="$HOME/Dropbox"
+DOTFILES="$DROPBOX_DIR/dotfiles"
+
+if [ ! -d "$DOTFILES" ]
+then
+echo "Please my dotfiles to $DROPBOX_DIR"
+  exit
+fi
+
+cd $DOTFILES
+
+git submodule update --init
+
+ln -s $DOTFILES/.gitconfig $HOME/.gitconfig
+ln -s $DOTFILES/.gvimrc $HOME/.gvimrc
+ln -s $DOTFILES/.tmux.conf $HOME/.tmux.conf
+ln -s $DOTFILES/.vim $HOME/.vim
+ln -s $DOTFILES/.vimrc $HOME/.vimrc
+ln -s $DOTFILES/.zshenv.mac $HOME/.zshenv.mac
+ln -s $DOTFILES/.zshrc $HOME/.zshrc
+
+vim -c ':NeoBundleInstall!'
