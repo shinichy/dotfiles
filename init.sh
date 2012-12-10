4a8279130676a952043ecc7d183ddcea9116ac1b
Shinichi Katayama <shin1.katayama@gmail.com>
2012-12-09 18:58:52 -0800

    some change

--- init.sh
+++ init.sh
@@ -14,16 +14,28 @@ cd $DOTFILES
 git submodule update --init
 
 ln -s $DOTFILES/.gitconfig $HOME/.gitconfig
-ln -s $DOTFILES/.tmux.conf $HOME/.tmux.conf
-ln -s $DOTFILES/.vim $HOME/.vim
-ln -s $DOTFILES/.vimrc $HOME/.vimrc
 ln -s $DOTFILES/.zshrc $HOME/.zshrc
 ln -s $DOTFILES/.ackrc $HOME/.ackrc
-if [ "`uname -s`" = "Darwin" ]; then
-	ln -s $DOTFILES/.zshenv.mac $HOME/.zshenv
-	ln -s $DOTFILES/.gvimrc $HOME/.gvimrc
-elif [ "`uname -s`" = "Linux" ]; then
-	ln -s $DOTFILES/.zshenv.linux $HOME/.zshenv
-elif echo "`uname -s`" | grep -q "CYGWIN"; then
+if echo "`uname -s`" | grep -q "CYGWIN"; then
+	cp -r $DOTFILES/.vim $HOME
+	cp $DOTFILES/.vimrc $HOME
+	nkf -s --overwrite $HOME/.vimrc
+	ln -s $DOTFILES/.screenrc $HOME/.screenrc
 	ln -s $DOTFILES/.zshenv.win $HOME/.zshenv
+	cp $DOTFILES/.vrapperrc $HOME
+else
+	ln -s $DOTFILES/.vim $HOME/.vim
+	ln -s $DOTFILES/.vimrc $HOME/.vimrc
+	ln -s $DOTFILES/.tmux.conf $HOME/.tmux.conf
+	if [ "`uname -s`" = "Darwin" ]; then
+		ln -s $DOTFILES/.zshenv.mac $HOME/.zshenv
+		ln -s $DOTFILES/.gvimrc $HOME/.gvimrc
+	elif [ "`uname -s`" = "Linux" ]; then
+		ln -s $DOTFILES/.zshenv.linux $HOME/.zshenv
+	fi
+fi
+
+# create vimswap dir
+if [ ! -d ~/.vimswap ]; then
+	mkdir ~/.vimswap
 fi
